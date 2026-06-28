ffd6ab99d8ab0d20b6825a040a6781d7ca5b6c11
user <pawel.m.janik@gmail.com>
2026-06-28 21:36:47 +0200

    Changed numberOfFigures to constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        int numberOfFigures = 6;
+        final int numberOfFigures = 6;
         Figure[] figures = new Figure[numberOfFigures];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < numberOfFigures; i++) {
